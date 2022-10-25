5af016f22dceddee8bbe00aa80a547bed779d8c8
Victoria <vic_figueroa08@yahoo.com>
2022-10-25 11:23:01 -0400

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("", redirect_to_list_projects, name="home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
