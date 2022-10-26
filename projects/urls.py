9cbc83bf75b3d1b380c159a4c4144a8d9b08e15f
Victoria <vic_figueroa08@yahoo.com>
2022-10-25 21:08:35 -0400

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from projects.views import list_projects
+from projects.views import list_projects, create_project
 
-urlpatterns = [path("", list_projects, name="list_projects")]
+urlpatterns = [
+    path("", list_projects, name="list_projects"),
+    path("create/", create_project, name="create_project"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -10,3 +11,20 @@ def list_projects(request):
     model_list = Project.objects.filter(owner=request.user)
     context = {"list_projects": model_list}
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            receipt = form.save(False)
+            receipt.purchaser = request.user
+            receipt.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create.html", context)
